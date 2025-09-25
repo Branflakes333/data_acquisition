6c8541a7c0502143bfdd7c7382e8c98652e86eed
Branflakes333 <brandonm333@outlook.com>
2025-09-25 14:50:01 -0700

    Auto backup 2025-09-25 14:50:01

--- Day8/Skeleton/ex01.py
+++ Day8/Skeleton/ex01.py
@@ -1,8 +1,17 @@
 from playwright.sync_api import sync_playwright
 
-# Step 1. Create a browser
-# Can use chromium/firefox/webkit
+with sync_playwright() as p:
+    # Step 1. Create a browser
+    # Can use chromium/firefox/webkit
+    browser = p.chromium.launch(headless=False)
+    # `headless` is for specifically seeing what the code is doing
 
-# Step 2. Create a new BrowserContext
+    # Step 2. Create a new BrowserContext
+    context = browser.new_context()
+    page = context.new_page()
 
-# Step 3. Open a page
+    # Step 3. Open a page
+    page.goto("https://reddit.com")
+    print(page.title())
+
+    browser.close()
--- Day8/Skeleton/ex02.py
+++ Day8/Skeleton/ex02.py
@@ -9,5 +9,10 @@ with sync_playwright() as p:
 
     # Waiting for Elements
     # Wait Until the <main> appears
+    page.wait_for_selector("main")
 
     # Select Elements
+    for anchor in page.query_selector_all("a"):
+        print(anchor.inner_text())
+
+    browser.close()
